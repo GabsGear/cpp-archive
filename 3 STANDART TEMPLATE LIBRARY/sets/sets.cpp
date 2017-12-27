d682516568e18ade8fa47b314dfa3d6442c27bd2
GabsGear <gabriel_ghellere@hotmail.com>
2017-12-27 15:26:45 -0200

    Sets included

---
+++ 3 STANDART TEMPLATE LIBRARY/sets/sets.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+class Test {
+    int id;
+    string name;
+
+public:
+    Test(int id, string name):id(id), name(name){ //constructor
+
+    }
+
+    void print() const {
+        cout << id << ": " << name << endl;
+    }
+};
+
+int main(){
+
+    set<int> numbers;
+
+    numbers.insert(50);
+    numbers.insert(10);
+    numbers.insert(20);
+    numbers.insert(20);
+    numbers.insert(25);
+
+    for(set<int>::iterator it = numbers.begin(); it != numbers.end(); it++){
+        cout << *it << endl;
+    }
+
+    set<int>::iterator find = numbers.find(20);
+    if(find != numbers.end()){
+        cout << *find << endl;
+    }
+
+    if(numbers.count(20)) {
+        cout << "numero encontrado" << endl;
+    }
+    else
+        cout << "numero nao encontrado" << endl;
+
+    set<Test> tests;
+    tests.insert(Test(10, "Gabs"));
+    tests.insert(Test(20, "jogr"));
+    tests.insert(Test(5, "carl"));
+
+    for(set<Test>::iterator it = tests.begin(); it != tests.end(); it++){
+        it->print();
+    }
+
+
+//set exibe ordenado sem elementos repetidos
+    return 0;
+}
